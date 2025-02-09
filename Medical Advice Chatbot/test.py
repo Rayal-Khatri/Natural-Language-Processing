bcd7e3a43982b3f856f96b083a60606700f596ea
Rayal Khatri <kcrayalgw@gmail.com>
2025-02-10 00:05:15 +0545

    Connection to LLM required

---
+++ Medical Advice Chatbot/test.py
@@ -0,0 +1,10 @@
+from langchain.embeddings import HuggingFaceEmbeddings
+
+
+def download_hugging_face_embeddings():
+    embeddings = HuggingFaceEmbeddings(model_name = "sentence-transformers/all-MiniLM-L6-v2")
+    return embeddings
+embeddings = download_hugging_face_embeddings()
+print(embeddings)
+
+
